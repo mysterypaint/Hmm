3e7d1ff6834383b8eb4c3f4a8dfb3f29bd4c097e
mysterypaint <mysterypainto@gmail.com>
2018-11-29 13:57:18 -0800

    Remove batch

--- build.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-del %~dp0\LaMulanaClassic3DS.3dsx
-make
-if exist %~dp0\out\LaMulanaClassic3DS.3dsx (
-	@echo "hi"
-    start C:\Users\User\AppData\Local\Citra\nightly-mingw\citra-qt.exe %~dp0\out\LaMulanaClassic3DS.3dsx
-)
